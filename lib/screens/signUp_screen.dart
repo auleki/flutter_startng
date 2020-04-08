bbfbe17dc55316241f0dfe4c5658a9d04a76ea7b
auleki <kodagiwa@gmail.com>
2020-04-09 02:25:38 +0400

    screens refactored to be external

---
+++ lib/screens/signUp_screen.dart
@@ -0,0 +1,209 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import '../utilities/constants.dart';
+
+class RegisterScreen extends StatefulWidget {
+  @override
+  _RegisterScreenState createState() => _RegisterScreenState();
+}
+
+class _RegisterScreenState extends State<RegisterScreen> {
+
+  Widget _buildEmailLTF() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Text(
+          'Email',
+          style: auLabelStyle,
+        ),
+        SizedBox(
+          height: 10.0,
+        ),
+        Container(
+            alignment: Alignment.centerLeft,
+            decoration: auBoxDecorationStyle,
+            height: 60.0,
+            child: TextField(
+              keyboardType: TextInputType.emailAddress,
+              style: TextStyle(color: Colors.white),
+              decoration: InputDecoration(
+                border: InputBorder.none,
+                contentPadding: EdgeInsets.only(top: 14.0),
+                prefixIcon: Icon(
+                  Icons.email,
+                  color: Colors.white,
+                ),
+                hintText: 'Type in your email',
+                hintStyle: auHintTextStyle,
+              ),
+            ))
+      ],
+    );
+  }
+
+  Widget _buildConfirmPassword() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Text(
+          'Confirm Password',
+          style: auLabelStyle,
+        ),
+        SizedBox(
+          height: 10.0,
+        ),
+        Container(
+            alignment: Alignment.centerLeft,
+            decoration: auBoxDecorationStyle,
+            height: 60.0,
+            child: TextField(
+              obscureText: true,
+              style: TextStyle(color: Colors.white),
+              decoration: InputDecoration(
+                border: InputBorder.none,
+                contentPadding: EdgeInsets.only(top: 14.0),
+                prefixIcon: Icon(
+                  Icons.lock,
+                  color: Colors.white,
+                ),
+                hintText: 'Type in your password',
+                hintStyle: auHintTextStyle,
+              ),
+            ))
+      ],
+    );
+  }
+
+  Widget _buildPasswordLTF() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Text(
+          'Password',
+          style: auLabelStyle,
+        ),
+        SizedBox(
+          height: 10.0,
+        ),
+        Container(
+            alignment: Alignment.centerLeft,
+            decoration: auBoxDecorationStyle,
+            height: 60.0,
+            child: TextField(
+              obscureText: true,
+              style: TextStyle(color: Colors.white),
+              decoration: InputDecoration(
+                border: InputBorder.none,
+                contentPadding: EdgeInsets.only(top: 14.0),
+                prefixIcon: Icon(
+                  Icons.lock,
+                  color: Colors.white,
+                ),
+                hintText: 'Type in your password',
+                hintStyle: auHintTextStyle,
+              ),
+            ))
+      ],
+    );
+  }
+
+
+  
+
+  Widget _buildSignUpBtn() {
+    return Container(
+      padding: EdgeInsets.symmetric(vertical: 25.0),
+      width: double.infinity,
+      child: RaisedButton(
+        elevation: 5.0,
+        onPressed: () => print('Login button was pressed'),
+        padding: EdgeInsets.all(15.0),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(30.0),
+        ),
+        color: Colors.white,
+        child: Text(
+          'SIGN UP',
+          style: TextStyle(
+            color: Color(0xFF527DAA),
+            letterSpacing: 1.5,
+            fontSize: 18.0,
+            fontWeight: FontWeight.bold,
+            fontFamily: 'OpenSans',
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildLoginBtn() {
+    return Center(
+        child: FlatButton(
+      onPressed: () => print("Redirect to Sign up page"),
+      child: Text(
+        'Already have an account? Login',
+        style: auLabelStyle,
+      ),
+    ));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: <Widget>[
+          Container(
+            height: double.infinity,
+            width: double.infinity,
+            decoration: BoxDecoration(
+                gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: [
+                Color(0xFF7B1FA2),
+                Color(0xFF8E24AA),
+                Color(0xFFAB47BC),
+                Color(0xFF9C27b0),
+                
+              ],
+              stops: [0.1, 0.4, 0.7, 0.9],
+            )),
+          ),
+          Container(
+            height: double.infinity,
+            child: SingleChildScrollView(
+              physics: AlwaysScrollableScrollPhysics(),
+              padding: EdgeInsets.symmetric(
+                horizontal: 40.0,
+                vertical: 120.0,
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    'Sign Up',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontFamily: 'OpenSans',
+                      fontSize: 30.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(height: 30.0),
+                  _buildEmailLTF(),
+                  SizedBox(height: 30.0),
+                  _buildPasswordLTF(),
+                  SizedBox(height: 30.0),
+                  _buildConfirmPassword(),                  
+                  _buildSignUpBtn(),
+                  _buildLoginBtn(),
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
